c8e275d0c91421a09602293e31168a5534705979
martinjuju <julien.ga.martin@gmail.com>
2021-05-21 10:42:36 -0400

    build

--- _build.sh
+++ _build.sh
@@ -3,4 +3,4 @@
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook', params = list(echo_sol = TRUE))"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book', params = list(echo_sol = FALSE))"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown:::epub_book')"
-Rscript -e "bookdown::calibre('docs/Labo_BIO4558.epub', 'mobi')"
+Rscript -e "bookdown::calibre('docs/wam_tuto.epub', 'mobi')"
