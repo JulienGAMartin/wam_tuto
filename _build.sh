111a8b7607dc98819fea33505b86effc50aef951
JulienGAMartin <julien.martin@uottawa.ca>
2022-11-29 16:53:12 -0500

    removing mobi format

--- _build.sh
+++ _build.sh
@@ -3,4 +3,3 @@
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook', params = list(echo_sol = TRUE))"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book', params = list(echo_sol = FALSE))"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown:::epub_book')"
-Rscript -e "bookdown::calibre('docs/wam_tuto.epub', 'mobi')"
