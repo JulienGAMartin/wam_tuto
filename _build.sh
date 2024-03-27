6ade483ec4b5367985aff65659a8a928fa94949f
JulienGAMartin <julien.martin@uottawa.ca>
2024-03-26 23:23:13 -0400

    multi-lang structure

--- _build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook', params = list(echo_sol = TRUE))"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book', params = list(echo_sol = FALSE))"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown:::epub_book')"
