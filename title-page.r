8da7eb93801b59a7cdf8efbb2215cb6851c3b2c1
JulienGAMartin <julien.martin@uottawa.ca>
2024-04-23 22:47:04 -0400

    adding a nicer pdf and more info to the preface

---
+++ title-page.r
@@ -0,0 +1,16 @@
+lang <- Sys.getenv("QUARTO_PROFILE")
+
+
+# Render template
+# Convert rendered PDF to PNG
+if (!file.exists(paste0("images/cover/cover_", lang, ".png"))){
+  library(tidyverse)
+  library(quarto)
+  library(magick)
+  
+pdf_file <- list.files(paste0("docs/"), pattern=".pdf", full.names = TRUE)
+image_read_pdf(pdf_file, pages = 1) |>
+  image_montage(geometry = "x1000+25+35", tile = 1, bg = "grey92", shadow = TRUE) |>
+  image_convert(format = "png") |>
+  image_write(paste0("images/cover/cover_", lang,".png"))
+}
