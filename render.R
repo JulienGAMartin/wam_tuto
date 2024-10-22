2bc893951d8362f9bd846cd21a95aa8121efd997
Julien Martin <julien.martin@uottawa.ca>
2024-10-21 20:37:51 -0400

    Delete render.R

--- render.R
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env Rscript
-rm(list = ls())
-bookdown::render_book("index.Rmd", "bookdown::gitbook", params = list(echo_sol = TRUE, html_pdf = TRUE))
-rm(list = ls())
-bookdown::render_book("index.Rmd", "bookdown::pdf_book", params = list(echo_sol = FALSE, html_pdf = FALSE))
-rm(list = ls())
-bookdown::render_book("index.Rmd", "bookdown::epub_book", params = list(echo_sol = FALSE, html_pdf = TRUE))
